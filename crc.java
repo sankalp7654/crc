94830572a714d6fde771a0a629945142c34124a6
Sankalp Saxena <sankalp.saxena.sta@gmail.com>
2018-10-30 02:09:42 +0530

    Update crc.java

--- crc.java
+++ crc.java
@@ -1,12 +1,11 @@
-package ComputerNetworksLabInternals;
 
 import java.util.Scanner;
 
 public class CRC {
 
+    static int codeword[];
     static int divisor[];
-    static int div[];
-    static int data[];
+    static int dataword[];
     static int remainder[];
     static int crc[];
     
@@ -14,34 +13,36 @@ public class CRC {
     {
         
         Scanner sc =new Scanner(System.in);
-        System.out.println("Enter the number of elements in data");
+        System.out.println("Enter the number of elements in dataword");
         int data_size=sc.nextInt();
-        data=new int[data_size];
-        System.out.println("Enter the data");
+        
+        dataword=new int[data_size];
+        System.out.println("Enter the dataword");
         for(int i=0;i<data_size;i++)
         {
-            data[i]=sc.nextInt();
+            dataword[i]=sc.nextInt();
         }
         
         System.out.println("Enter the number of elements in divisor");
         int div_size=sc.nextInt();
-        div=new int[div_size];
+        
+        divisor=new int[div_size];
         System.out.println("Enter the divisor");
         for(int i=0;i<div_size;i++)
         {
-            div[i]=sc.nextInt();
+            divisor[i]=sc.nextInt();
         }
         
         
-        int total_length=data.length +div.length-1;
-        divisor=new int[total_length];
+        int total_length=dataword.length +divisor.length-1;
+        codeword=new int[total_length];
         remainder=new int[total_length];
         crc=new int[total_length];
         
-        for(int i=0;i<data.length;i++)
+        for(int i=0;i<dataword.length;i++)
         {
-            divisor[i]=data[i];
-            remainder[i]=data[i];
+            codeword[i]=dataword[i];
+            remainder[i]=dataword[i];
         }
         
         //System.out.println(div);
@@ -49,11 +50,11 @@ public class CRC {
         //System.out.println(remainder);
         
         
-        remainder=divide(divisor,div,remainder);
+        remainder=divide(codeword,divisor,remainder);
         
         for(int i=0;i<remainder.length;i++)
         {
-            crc[i]=remainder[i]^divisor[i];
+            crc[i]=remainder[i]^codeword[i];
             
         
         }
@@ -73,31 +74,32 @@ public class CRC {
         for(int i=0;i<crc.length;i++)
             remainder[i]=crc[i];
         
-        remainder=divide(crc, div, remainder);
+        remainder=divide(crc, divisor, remainder);
         
+        int flag = 1;
         for(int i=0;i<remainder.length;i++)
         {
             if(remainder[i]!=0)
             {
                 System.out.println("Error detected");
-            }
-            if(i==remainder.length-1)
-            {
-                System.out.println("No error");
+                flag = 0;
+                break;
             }
         }
+        if(flag == 1)
+        	System.out.println("No error detected");
         
     }
 
-    private static int[] divide(int[] divisor, int[] div, int[] remainder) {
+    private static int[] divide(int[] codeword, int[] divisor, int[] remainder) {
         
         int count=0;
         
         while(true)
         {
-            for(int i=0;i<div.length;i++)
+            for(int i=0;i<divisor.length;i++)
             {
-                remainder[count+i]=remainder[count+i]^div[i];
+                remainder[count+i]=remainder[count+i]^divisor[i];
             }
             
             while(remainder[count]==0 && count!=remainder.length-1)
@@ -105,7 +107,7 @@ public class CRC {
                 count++;
             }
             
-            if(((remainder.length)-count)<div.length)
+            if(((remainder.length)-count)<divisor.length)
                 break;
         }
         return remainder;
